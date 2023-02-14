11b3d9b8978f8364183ddfa605c481ae498af555
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-14 12:58:46 -0300

    header layout

--- pomodor-timer/src/components/Header/index.tsx
+++ pomodor-timer/src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import { HeaderContainer } from './styles'
-
+import { NavLink } from 'react-router-dom'
+import mainlogo from '../../assets/mainlogo.svg'
+import { Timer, Scroll } from 'phosphor-react'
 export function Header() {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={mainlogo} alt="logo sorrindo" />
       <nav>
-        <a href="">TIMER</a>
-        <a href="">HISTORY</a>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
--- pomodor-timer/src/components/Header/styles.ts
+++ pomodor-timer/src/components/Header/styles.ts
@@ -3,4 +3,35 @@ export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      color: ${props => props.theme['yellow-700']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${props => props.theme['yellow-500']};
+      }
+
+      &:active {
+        box-shadow: 0 0 10px #ffd700;
+      }
+    }
+  }
+
+  img {
+    width: 3rem;
+    height: 3rem;
+  }
 `
