704b18827ee3c2360f4a012178b7adfcc02bc609
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-13 21:27:53 -0300

    add styled components and use variables styles

--- pomodor-timer/src/App.tsx
+++ pomodor-timer/src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from './components/Button'
+
 export function App() {
-  return <h1>hello world</h1>
+  return (
+    <>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="success"></Button>
+    </>
+  )
 }
