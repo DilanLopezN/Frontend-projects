6eef6ad6f11b09d831c7ddb0608d67d4bce6cfaf
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-14 12:13:11 -0300

    add navigation with react router dom

--- pomodor-timer/src/App.tsx
+++ pomodor-timer/src/App.tsx
@@ -1,16 +1,16 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="danger"></Button>
-      <Button variant="success"></Button>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
