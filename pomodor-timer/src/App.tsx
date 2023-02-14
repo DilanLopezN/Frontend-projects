0d1ce39c06bbadf71aa7bf63780a68a651d8d3cc
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-13 21:43:30 -0300

    global styles

--- pomodor-timer/src/App.tsx
+++ pomodor-timer/src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Button variant="primary"></Button>
       <Button variant="secondary"></Button>
       <Button variant="danger"></Button>
