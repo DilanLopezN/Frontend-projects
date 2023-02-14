d6078d0cc4d0610f2d37579f0d748481ff6fd044
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-14 12:29:49 -0300

    header base layout

---
+++ pomodor-timer/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <a href="">TIMER</a>
+        <a href="">HISTORY</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
