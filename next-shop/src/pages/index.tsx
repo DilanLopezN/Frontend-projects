ad859032ed3beb74e8c242c1c951fb82c73d8384
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-15 19:17:50 -0300

    Home base layout finished

--- next-shop/src/pages/index.tsx
+++ next-shop/src/pages/index.tsx
@@ -1,9 +1,28 @@
-import { styled } from '../styles'
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  padding: 0
-})
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
+import estampa1 from '../assets/1.png'
+import estampa2 from '../assets/2.png'
+import estampa3 from '../assets/3.png'
+
 export default function Home() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={estampa1} width={520} height={480} alt="estampas" />
+
+        <footer>
+          <strong>Estampa X</strong>
+          <span>R$ 49,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={estampa2} width={520} height={480} alt="estampas" />
+
+        <footer>
+          <strong>Estampa Y</strong>
+          <span>R$ 69,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
