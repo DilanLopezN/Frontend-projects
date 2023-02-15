3bd5264093aa3feab630db6059a4fca719091a8e
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-15 18:03:45 -0300

    global styles

--- next-shop/src/pages/index.tsx
+++ next-shop/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { styled } from '../styles'
 const Button = styled('button', {
-  backgroundColor: 'purple'
+  backgroundColor: '$green300',
+  borderRadius: 4,
+  padding: 0
 })
 export default function Home() {
   return <Button>Enviar</Button>
