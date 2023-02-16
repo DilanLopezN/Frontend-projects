5e8f85bed7e53270bc32ea40deb752956fdce51e
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-16 15:28:07 -0300

    add dinamic sgg product page

--- next-shop/src/pages/products/[id].tsx
+++ next-shop/src/pages/products/[id].tsx
@@ -1,27 +1,63 @@
+import { stripe } from '@/lib/stripe'
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails
 } from '@/styles/pages/product'
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
-
-export default function Product() {
-  const { query } = useRouter()
+import Stripe from 'stripe'
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+    description: string
+  }
+}
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image width={520} height={480} src={product.imageUrl} alt="" />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At officia,
-          minus id minima inventore, aliquid voluptate amet dignissimos qui
-          ipsum veritatis sunt repudiandae eligendi! Non id nesciunt porro
-          expedita veniam?
-        </p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params
+}) => {
+  const productId = params!.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        url: product.url,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount! / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 2 // 2 horas
+  }
 }
