3698ce19d1763f8cae814c0643acc48a42ab89cb
Dilan lopez <dilanlopez009@gmail.com>
2023-09-13 09:11:53 -0300

    base with redux

---
+++ redux-zustand/src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: ['fazer café', 'estudar redux'],
+  reducers: {}
+})
+export const store  = configureStore({
+  reducer: {
+    todo: todoSlice.reducer
+  }
+})
