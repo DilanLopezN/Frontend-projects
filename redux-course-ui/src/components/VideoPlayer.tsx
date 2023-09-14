113cd5103d0e19777703ff9eb80de4eac706fa6a
Dilan lopez <dilanlopez009@gmail.com>
2023-09-14 11:19:29 -0300

    reduce auto play next lessons/modules

--- redux-course-ui/src/components/VideoPlayer.tsx
+++ redux-course-ui/src/components/VideoPlayer.tsx
@@ -1,5 +1,7 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store'
+import { next } from '../store/slices/Player'
+import { useDispatch } from 'react-redux'
 export function VideoPlayer() {
 
   const lesson = useAppSelector(state => {
@@ -9,13 +11,21 @@ export function VideoPlayer() {
 
     return currentLesson
   })
+
+  const dispatch = useDispatch()
+
+  const handlePlayNext = () => {
+    dispatch(next())
+  }
   return (
     <div className="w-full bg-zinc-950 aspect-video">
     <ReactPlayer
       width="100%"
       height="100%"
       controls
+      playing
       url={`https://www.youtube.com/watch?v=${lesson.id}`}
+      onEnded={handlePlayNext}
     />
   </div>  
   )
